ec7ff1c0b96546fb69ce592a9f543739836156b4
TenserFlow <rosemissed@tutanota.com>
2025-04-26 14:22:50 +0300

    input_output_and_size_training

--- go.go
+++ go.go
@@ -1,7 +1,23 @@
-package main  // special package for entry point 
+package main // special package for entry point
 
-import "fmt" // importing fmt module for I/O 
+import(
+	"fmt"
+	"unsafe"
+)
 
 func main() { // entry point
-    fmt.Println("Hello, world.") // output 
+
+	
+	var _name1 string;
+
+	 _name1 = "idk"
+	// var (
+	// 	firstName string = "Alex"
+	// 	lastName string = "Skyba"
+	// 	year int = 1000;
+	// )
+	fmt.Println("Hello, world.") // output
+	fmt.Println(_name1);
+	fmt.Println("Size of int is ", unsafe.Sizeof(int(0)))
+
 }
