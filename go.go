165c3b8e6677b97979c8a6bf3f6ac4d3195f4b78
TenserFlow <rosemissed@tutanota.com>
2025-04-26 14:29:14 +0300

    output_input_training

--- go.go
+++ go.go
@@ -1,23 +1,23 @@
 package main // special package for entry point
 
-import(
+import (
 	"fmt"
 	"unsafe"
 )
 
 func main() { // entry point
 
-	
-	var _name1 string;
+	var _name1 string             // first method
+	_new := "I really don't know" // second method
+	var _name2 string = "smth"    // third method
 
-	 _name1 = "idk"
+	_name1 = "idk"
 	// var (
 	// 	firstName string = "Alex"
 	// 	lastName string = "Skyba"
 	// 	year int = 1000;
 	// )
-	fmt.Println("Hello, world.") // output
-	fmt.Println(_name1);
+	fmt.Println("\n This is name 1 -> ", _name1,  " \n This is the name 2 -> ", _new,  " \n This is the name 3 -> ", _name2)
 	fmt.Println("Size of int is ", unsafe.Sizeof(int(0)))
 
 }
