8f9e5c6b832af5776c3bdde270235108744c603a
tensa <rosemissed@tutanota.com>
2025-05-10 14:31:41 +0300

    exers

--- loops.go
+++ loops.go
@@ -20,9 +20,50 @@ func main() {
 	fmt.Println("LOOOPS")
 	fmt.Println("--------------------------------------------------------------")
 
+	sliice :=[]string{"Alex", "Nikita", "Andrey"}
 
-	for i := 0; i < 3; i++ {
-		a := i+3
-		fmt.Println("Iteration error", i, i, i, a)
+	// i := 0
+	// for  i < 3 {
+	// 	a := i+3
+	// 	fmt.Println("Iteration ", i, i, i, a)
+	// }
+
+	for i := 0; i < len(sliice); i++{
+
+		fmt.Println(sliice[i], i )
 	}
+	outerLoop:
+	for i := 0; i < 4; i++{
+		for j:= 0; j < 4; j++{
+			fmt.Println("Error")
+			break outerLoop
+		}
+
+	}
+	for i := 0; i < 10; i++{
+		if i % 2 == 0{
+			continue
+		}else{
+			fmt.Println("Even", i)
+		}
+	}
+
+
+	for i := 2; i < 10; i++ {
+		
+		if 21%i == 0  || i / 9 == 1{
+			fmt.Println(i)
+		}
+	}
+
+
+	fmt.Println("New ")
+	for i := 0; i < 10; i++ {
+    if i > 4 && i < 7 {
+        continue
+    }
+    fmt.Println("число:", i)
 }
+
+}
+
