a5996679178db6c6d302132b0b6272eb48be18e9
tensa <rosemissed@tutanota.com>
2025-05-24 14:09:30 +0300

    wha1

--- lesson5/main.go
+++ lesson5/main.go
@@ -5,17 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Yo")
-	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
-	}
+	arr := [5]int{1, 2, 3, 4, 5}
 
-	var fr [5]string = [5]string{"orange", "banana", "peach", "apple", "grapes"}
+	slice1 := arr[1:4]
+	slice2 := arr[0:3]
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(fr[i])
-	}
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 
 }
